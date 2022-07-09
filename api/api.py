32bbd87582d7906f382d84293ea414df30f56a41
Quintin Russell <quinn.j.russell@gmail.com>
2022-07-09 10:36:04 -0700

    cors error

--- api/api.py
+++ api/api.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/hi')
 def say_hi():
-    return {"message": "sup stoker"}
+    resp = Flask.Response({"message": "sup stoker"})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return jsonify(resp)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [greeting, setGreeting] = useState({message: 'not recieved'})
+
+  useEffect(()=>{
+    // const fetchData = async () => {
+    //   const apiData = await fetch('/hi')
+    //   const parsedData = await apiData.json()
+    //   setGreeting(parsedData)
+    // }
+    // fetchData()
+    //   .catch(err => console.log(err))
+    fetch('http://localhost:5000/hi')
+      .then(res => res.json())
+      .then(data => setGreeting(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Message: {greeting.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
